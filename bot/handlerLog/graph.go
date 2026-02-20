958d257c7e24dd721430becb645e52e1b3e45112
Anton Kim <tatoshko@gmail.com>
2026-02-20 14:16:04 +0300

    auto

--- bot/handlerLog/graph.go
+++ bot/handlerLog/graph.go
@@ -58,7 +58,7 @@ func Graph(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 
     filename := fmt.Sprintf("%s_%d.png", time.Now().Format("2006_01_02"), userID)
     logger(filename)
-    render.MakeSnapshot(render.NewSnapshotConfig(line.RenderContent(), filename, func(config *render.SnapshotConfig) {
+    render.MakeSnapshot(render.NewSnapshotConfig(line.RenderContent(), "yest.jpg", func(config *render.SnapshotConfig) {
         config.Path = "charts"
     }))
 
